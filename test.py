64ea22c3eb4a2027f80d4285defb1ab42746d0ab
sleepymalon <evalbouys@gmail.com>
2021-10-26 12:55:51 +0200

    exercice 9

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-def test():
-    return (0)
-e
+def maxsearch(list):
+    if (len(list) == 1):
+        return (list[0])
+    first = list[0]
+    max = maxsearch(list[1:])
+    if (first < max):
+        return (max)
+    else:
+        return (first)
+
+print (maxsearch([1, 89, 12, 55]))
